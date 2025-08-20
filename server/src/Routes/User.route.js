852fec1ce9276f2799ba76240e3310bb1664b635
madhavbanjade <madhavbanjade005@gmail.com>
2025-08-20 18:39:44 +0545

    file

--- server/src/Routes/User.route.js
+++ server/src/Routes/User.route.js
@@ -1,19 +1,19 @@
-import { Router } from "express";
-import {
-  getUsers,
-  login,
-  register,
-  updateProfile,
-} from "../controller/User.controller.js";
-import { Protected } from "../middleware/Protected.js";
-import { upload } from "../middleware/multer/userMulter.js";
+  import { Router } from "express";
+  import {
+    getUsers,
+    login,
+    register,
+    updateProfile,
+  } from "../controller/User.Controller.js";
+  import { Protected } from "../middleware/Protected.js";
+  import { upload } from "../middleware/multer/userMulter.js";
 
-const userRouter = Router();
-userRouter.route("/register").post(register);
-userRouter.route("/login").post(login);
-userRouter
-  .route("/update-profile/:id")
-  .put(Protected, upload.single("avatar"), updateProfile);
-userRouter.route("/get-users").get(Protected, getUsers);
+  const userRouter = Router();
+  userRouter.route("/register").post(register);
+  userRouter.route("/login").post(login);
+  userRouter
+    .route("/update-profile/:id")
+    .put(Protected, upload.single("avatar"), updateProfile);
+  userRouter.route("/get-users").get(Protected, getUsers);
 
-export default userRouter;
+  export default userRouter;
