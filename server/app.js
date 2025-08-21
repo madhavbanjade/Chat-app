56fdce004394f7115072aa4eb6e775bbeac79881
madhavbanjade <madhavbanjade005@gmail.com>
2025-08-21 16:35:56 +0545

    no credentails

--- server/app.js
+++ server/app.js
@@ -29,7 +29,6 @@ app.use(
       if (!origin || allowedOrigins.includes(origin)) callback(null, true);
       else callback(new Error("Not allowed by CORS"));
     },
-    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
