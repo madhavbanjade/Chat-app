f27aaf5592f5ab2304e5583e9905dac7528fe2bc
madhavbanjade <madhavbanjade005@gmail.com>
2025-08-28 23:26:02 +0545

    socket

--- client/src/components/socket/Socket.jsx
+++ client/src/components/socket/Socket.jsx
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
 
 // 🔹 Replace with your backend server URL
-// const SOCKET_URL = "https://chat-app-q8w9.onrender.com";
-const SOCKET_URL = "http://localhost:3000";
+const SOCKET_URL = "https://chat-app-q8w9.onrender.com";
+// const SOCKET_URL = "http://localhost:3000";
 
 // Create socket connection
 const socket = io(SOCKET_URL, {
